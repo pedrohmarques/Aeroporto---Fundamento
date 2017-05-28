a6eca22754e7120269f3a5b55f8e834663efb88f
pedrohmarques <pdhmarques@gmail.com>
2017-05-28 11:42:28 -0300

    Add files via upload
    Falta Terminar a parte A

--- Aeporto.c
+++ Aeporto.c
@@ -5,5 +5,23 @@
 //Mudar nome depois
 int main()
 {
-
+  char menu;
+  int NumeroVoo[50];
+  int i,reserva=1000,assento=1;
+  for(i=0;i<50;i++)
+  {
+  	NumeroVoo[i] = reserva;
+	reserva++;
+  }
+   
+  do{
+  
+  printf("\n\nMenu:\n1 - Inicializar Dados\n2 - Incluir Reserva\n3 - Excluir Reserva\n4 - Imprimir Reserva\n5 - Sair\n");
+  menu = getch();
+  switch (menu){
+  	case '1': InicializaDados(50,NumeroVoo);
+	break;
+  }
+  }while(menu!='5');
 }
+
--- funcao.h
+++ funcao.h
@@ -1,5 +1,6 @@
 #ifndef FUNCAO_H_INCLUDED
 #define FUNCAO_H_INCLUDED
+#include <time.h>
 
 //  Aqui irão ficar nossas funções
 /* EXEMPLO:
@@ -10,6 +11,27 @@ int Reserva()
     return 0;
 }*/
 
+int Gerador_CPF()
+{
+	int i,cpfnum[10];
+	srand(time(NULL));
+	for (i = 0; i <= 8; i++) {
+	cpfnum[i] = rand() % 9;
+	printf("%d",cpfnum[i]);
+}
+
+}
+
+int InicializaDados(int x, int *NumeroVoo)
+{
+int i;
+printf("\nInformacao de Voo:",NumeroVoo[i],i);
+for(i=0;i<50;i++)
+  {
+  	printf("\n%d",NumeroVoo[i]);
+  	
+  }	
+}
 
 
 #endif // FUNCAO_H_INCLUDED
