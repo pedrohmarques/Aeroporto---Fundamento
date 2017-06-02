1a32b080c5bf8b8bb036915ac45c8d3c8c37951f
pedrohmarques <pdhmarques@gmail.com>
2017-06-02 12:02:11 -0300

    Add files via upload
    Projeto completo

--- Aeporto.c
+++ Aeporto.c
@@ -3,47 +3,52 @@
 #include "funcao.h"
 int main()
 {
-  char menu;
-  int Voo[50][12];
-  int i,j,cpf;
-  for(i=0;i<10;i++)
-  {
-    Voo[i][0]=1000+i;
-  	Voo[i+10][0]=2000+i;
-  	Voo[i+20][0]=3000+i;
-  	Voo[i+30][0]=4000+i;
-  	Voo[i+40][0]=5000+i;
-  }
+    char menu;
+    int Voo[50][12];
+    int i,j,cpf;
+    for(i=0; i<10; i++)
+    {
+        Voo[i][0]=1000+i;
+        Voo[i+10][0]=2000+i;
+        Voo[i+20][0]=3000+i;
+        Voo[i+30][0]=4000+i;
+        Voo[i+40][0]=5000+i;
+    }
 
-  for(i=0;i<40;i++)
-  {
-      Voo[i][11]=10;
-      for(j=1;j<10;j++)
-      {
+    for(i=0; i<40; i++)
+    {
+        Voo[i][11]=10;
+        for(j=1; j<10; j++)
+        {
             cpf = Gerador_CPF();
             if(cpf<0)
             {
-            	Voo[i][j]= 0;
-			}else{
-				Voo[i][j]=cpf;
-			}
-      }
+                Voo[i][j]=0;
+            }
+            else
+            {
+                Voo[i][j]=cpf;
+            }
+        }
 
- }
+    }
 
-
-  do{
-
-  printf("\n\nMenu:\n1 - Inicializar Dados\n2 - Incluir Reserva\n3 - Excluir Reserva\n4 - Imprimir Reserva\n5 - Sair\n");
-  menu = getch();
-  switch (menu){
-  	case '1': InicializaDados(Voo);
-	break;
-	case '3': ExcluirReserva(Voo);
-	break;
-	case '4': ImprimirReserva(Voo);
-	break;
-  }
-  }while(menu!='5');
+    do
+    {
+        printf("\n\nMenu:\n1 - Inicializar Dados\n2 - Incluir Reserva\n3 - Excluir Reserva\n4 - Imprimir Reserva\n5 - Sair\n");
+        menu = getch();
+        switch (menu)
+        {
+        case '1':InicializaDados(Voo);
+        break;
+        case '2': IncluirReserva(Voo);
+        break;
+        case '3': ExcluirReserva(Voo);
+        break;
+        case '4': ImprimirReserva(Voo);
+        break;
+        }
+    }
+    while(menu!='5');
 }
 
--- funcao.h
+++ funcao.h
@@ -19,7 +19,7 @@ int Gerador_CPF()
 
     for(i = 0; i < 8; i++)
     {
-        cpf = (rand() % 9-0)+cpf*10;
+        cpf = (rand() % 9)+cpf*10;
     }
     return cpf;
 }
@@ -38,7 +38,7 @@ int InicializaDados(int Voo[50][12])
     }
 }
 
-int ExcluirReserva(int Voo[50][12])
+int ExcluirReserva (int Voo[50][12])
 {
     int n,i,j,a,b,c,cpf2;
     a=0;
@@ -69,8 +69,122 @@ int ExcluirReserva(int Voo[50][12])
     {
         printf("\nERRO: Codigo 0");
     }
-
 }
+
+int IncluirReserva(int Voo[50][12])
+{
+    int numVoo,i,cliCPF,j,assento,error,cont;
+    cont=0;
+    error=0;
+    printf("Digite o numero do voo: ");
+    scanf("%d",&numVoo);
+
+    for(i=0; i<50; i++)
+    {
+        if (numVoo==Voo[i][0])
+        {
+            printf("Digite seu CPF: ");
+            scanf("%d",&cliCPF);
+            error=1;
+        }
+    }
+
+    if(error==0)
+    {
+        printf("ERROR: CODIGO 0 - Voo nao existe");
+    }
+    else
+    {
+
+        for(i=0; i<50; i++)
+        {
+            if(numVoo==Voo[i][0])
+            {
+                for(j=1; j<11; j++)
+                {
+                    if(Voo[i][j]!=0)
+                    {
+                        cont++;
+                    }
+                }
+            }
+        }
+
+
+        if(cont==10)
+        {
+            printf("ERROR: CODIGO 1 - Voo lotado");
+        }
+        else
+        {
+            cont =0;
+            for(i=0; i<50; i++)
+            {
+                if(numVoo==Voo[i][0])
+                {
+                    for(j=1; j<11; j++)
+                    {
+                        if(Voo[i][j]==cliCPF)
+                        {
+                            cont++;
+                        }
+                    }
+                }
+            }
+            if(cont==1)
+            {
+                printf("ERROR: CODIGO 2 - CPF Reservado");
+            }
+            else
+            {
+                printf("Assentos: ");
+                for(i=0; i<50; i++)
+                {
+                    if(numVoo==Voo[i][0])
+                    {
+                        for(j=1; j<11; j++)
+                        {
+                            printf("\nA%d: %d ",j,Voo[i][j]);
+                        }
+                    }
+                }
+
+
+
+                printf("\n\nDigite o Assento");
+                printf("\nAssento: A");
+                scanf("%d",&assento);
+
+                for(i=0; i<50; i++)
+                {
+                    if(numVoo==Voo[i][0])
+                    {
+                        if(Voo[i][assento] == 0)
+                        {
+                            Voo[i][assento] = cliCPF;
+                            printf("Reserva feita com sucesso!");
+                        }
+                        else
+                        {
+                            printf("ERROR: CODIGO 3 - Assento Indisponivel");
+                        }
+                    }
+                }
+
+                for(i=0; i<50; i++)
+                {
+                    if(numVoo==Voo[i][0])
+                    {
+
+                        printf("\nA%d: %d ",assento,Voo[i][assento]);
+
+                    }
+                }
+            }
+        }
+    }
+}
+
 int ImprimirReserva (int Voo[50][12])
 {
     int n,i,j,contVaga,contReser,erro;
@@ -111,7 +225,6 @@ int ImprimirReserva (int Voo[50][12])
         printf("Vagas: %d\nReservas: %d",contVaga,contReser);
     }
 
-
-
 }
+
 #endif // FUNCAO_H_INCLUDED
